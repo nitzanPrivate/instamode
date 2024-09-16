f6a5a4bbac649fc42ab38b2c362788b10fe74dcd
nitzan <nitzanp@hackeruso.com>
2024-09-16 19:13:00 +0300

    clean up the tore

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,14 @@
 import { createStore, combineReducers } from 'redux'
 
-import { carReducer } from './car.reducer'
-import { boardReducer } from './board.reducer'
 import { userReducer } from './user.reducer'
-import { reviewReducer } from './review.reducer'
 import { systemReducer } from './system.reducer'
 
 const rootReducer = combineReducers({
-    carModule: carReducer,
-    boardModule: boardReducer,
     userModule: userReducer,
     systemModule: systemReducer,
-    reviewModule: reviewReducer,
 })
 
-
-const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
+const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
 export const store = createStore(rootReducer, middleware)
 
 // For debug:
@@ -24,6 +17,3 @@ export const store = createStore(rootReducer, middleware)
 //     console.log('storeState:\n', store.getState())
 //     console.log('*******************************')
 // })
-
-
-
