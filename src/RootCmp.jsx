12909122b1efaedbbbfa9a4148bb7b1ccf562373
nitzan <nitzanp@hackeruso.com>
2024-09-16 18:55:10 +0300

    fixed the file to support the clean project setup

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -3,16 +3,9 @@ import { Routes, Route } from 'react-router'
 
 import { HomePage } from './pages/HomePage'
 import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
-import { BoardIndex } from './pages/BoardIndex.jsx'
-import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminIndex } from './pages/AdminIndex.jsx'
-
-import { CarDetails } from './pages/CarDetails'
 import { UserDetails } from './pages/UserDetails'
-import { BoardDetails } from './pages/BoardDetails'
-import { TaskDetails } from './pages/TaskDetails'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
@@ -28,14 +21,7 @@ export function RootCmp() {
                         <Route path="team" element={<AboutTeam />} />
                         <Route path="vision" element={<AboutVision />} />
                     </Route>
-                    <Route path="car" element={<CarIndex />} />
-                    <Route path="car/:carId" element={<CarDetails />} />
                     <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="board" element={<BoardIndex />} />
-                    <Route path="board/:boardId" element={<BoardDetails />} >
-                        <Route path="group/:groupId/task/:taskId" element={<TaskDetails />} />
-                    </Route>
-                    <Route path="review" element={<ReviewIndex />} />
                     <Route path="chat" element={<ChatApp />} />
                     <Route path="admin" element={<AdminIndex />} />
                 </Routes>
@@ -44,5 +30,3 @@ export function RootCmp() {
         </div>
     )
 }
-
-
