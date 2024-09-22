f006165b42cc102db01f6075613f659a6c3bd00a
nitzan <nitzanp@hackeruso.com>
2024-09-22 08:11:59 +0300

    testing

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -17,40 +17,25 @@ const isLocalhost = Boolean(
       // 127.0.0.0/8 are considered localhost for IPv4.
       window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
   );
-  
+
   export function register(config) {
-    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-      // The URL constructor is available in all browsers that support SW.
-      const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-      if (publicUrl.origin !== window.location.origin) {
-        // Our service worker won't work if PUBLIC_URL is on a different origin
-        // from what our page is served on. This might happen if a CDN is used to
-        // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-        return;
-      }
-  
-      window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-  
-        if (isLocalhost) {
-          // This is running on localhost. Let's check if a service worker still exists or not.
-          checkValidServiceWorker(swUrl, config);
-  
-          // Add some additional logging to localhost, pointing developers to the
-          // service worker/PWA documentation.
-          navigator.serviceWorker.ready.then(() => {
-            console.log(
-              'This web app is being served cache-first by a service ' +
-                'worker. To learn more, visit https://cra.link/PWA'
-            );
-          });
-        } else {
-          // Is not localhost. Just register service worker
-          registerValidSW(swUrl, config);
-        }
-      });
+    if ('serviceWorker' in navigator) {
+        // Define the service worker URL based on the current environment
+        const swUrl = `${window.location.origin}/instamode/service-worker.js`; 
+
+        window.addEventListener('load', () => {
+            if (process.env.NODE_ENV === 'production') {
+                // Register the service worker for production
+                registerValidSW(swUrl, config);
+            } else {
+                // For localhost, check if a service worker already exists
+                checkValidServiceWorker(swUrl, config);
+            }
+        });
     }
-  }
+}
+
+
   
   function registerValidSW(swUrl, config) {
     navigator.serviceWorker
