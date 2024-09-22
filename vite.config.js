ec9f0341708d8c9e4eb7a0995b11194af5e4affe
nitzan <nitzanp@hackeruso.com>
2024-09-22 07:50:09 +0300

    fix local env for view the changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: '/instamode/',  // for GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/instamode/' : '/',  // Use empty string for local development
   plugins: [react(), svgr()]
 })
