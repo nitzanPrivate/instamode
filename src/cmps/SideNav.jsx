d5c74c3fff24baaea66b73f42378962763a1b220
nitzan <nitzanp@hackeruso.com>
2024-09-22 11:02:14 +0300

    added a side menu list

---
+++ src/cmps/SideNav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function SideNav() {
+    return (
+        <nav className="side-nav">
+            <ul>
+                <li>Home</li>
+                <li>Search</li>
+                <li>Explore</li>
+                <li>Reels</li>
+                <li>Messages</li>
+                <li>Notifications</li>
+                <li>Create</li>
+                <li>Profile</li>
+                <li>Threads</li>
+                <li>More</li>
+            </ul>
+        </nav>
+    );
+}
