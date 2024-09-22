f80f56906e15d2c5c0373de19250d4299cac59cd
nitzan <nitzanp@hackeruso.com>
2024-09-22 11:02:00 +0300

    add defualt view for post list cmps

---
+++ src/cmps/PostsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function PostsList() {
+    return (
+        <div>
+            {/* Render your posts here */}
+            <h2>Posts List</h2>
+        </div>
+    );
+}
