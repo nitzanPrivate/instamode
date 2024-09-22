7fa2ff26b0d503bee16bd05cc70d28940e2dbd8c
nitzan <nitzanp@hackeruso.com>
2024-09-22 06:24:00 +0300

    installed vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   base: '/instamode/',  // for GitHub Pages
-  plugins: [react()]
+  plugins: [react(), svgr()]
 })
