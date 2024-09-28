b00fa10bf0980305794f0df40e91fcd38ca7ab69
nitzan <nitzanp@hackeruso.com>
2024-09-28 17:11:54 +0300

    added upload to post

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { SideNav } from '../cmps/SideNav'; 
 import { TopBar } from '../cmps/TopBar'; 
 import { PostsList } from '../cmps/PostsList'; 
+import { ImgUploader} from '../cmps/ImgUploader';
 
 export function HomePage() {
+    const handleImageUpload = (url) => {
+        console.log('Image uploaded successfully:', url);
+    };
     return (
         <section className="home-page">
             <div className="main-layout">
@@ -11,6 +15,7 @@ export function HomePage() {
                 <div className="posts-section">
                     <TopBar />
                     <PostsList />
+                    <ImgUploader onUploaded={handleImageUpload} />
                 </div>
             </div>
         </section>
