a50782a2fe221f4f26b491ac63e20b6add8d9312
nitzan <nitzanp@hackeruso.com>
2024-09-28 17:12:48 +0300

    added links to icons from cloudinary

---
+++ src/assets/svg/sideMenuIcons.js
@@ -0,0 +1,13 @@
+const sideMenuIcons = {
+    homeIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727529739/Mediamodifier-Design-Template_niaw3b.jpg",
+    searchIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531073/searchIcon_kfhrhp.jpg",
+    exploreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/exploreIcon_jowccy.jpg",
+    reelsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/reelzIcon_hjm9pg.jpg",
+    messagesIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/msgIcon_ewypb9.jpg",
+    notificationsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531493/notificationIcon_b9nbe9.jpg",
+    createIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531491/createIcon_rg9yc6.jpg",
+    profileIcon: "",
+    moreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/moreIcon_fwtgff.jpg",
+};
+
+export default sideMenuIcons;
