92b2fd351e949d4d16e980d96568e86b31ee70e5
nitzan <nitzanp@hackeruso.com>
2024-09-30 16:50:14 +0300

    fixed the sideMenu icons and UI

--- src/assets/svg/sideMenuIcons.js
+++ src/assets/svg/sideMenuIcons.js
@@ -1,13 +1,13 @@
 const sideMenuIcons = {
-    homeIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727529739/Mediamodifier-Design-Template_niaw3b.jpg",
-    searchIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531073/searchIcon_kfhrhp.jpg",
-    exploreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/exploreIcon_jowccy.jpg",
-    reelsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/reelzIcon_hjm9pg.jpg",
-    messagesIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/msgIcon_ewypb9.jpg",
-    notificationsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531493/notificationIcon_b9nbe9.jpg",
-    createIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531491/createIcon_rg9yc6.jpg",
-    profileIcon: "",
-    moreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/moreIcon_fwtgff.jpg",
+    // homeIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727529739/Mediamodifier-Design-Template_niaw3b.jpg?react",
+    // searchIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531073/searchIcon_kfhrhp.jpg?react",
+    // exploreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/exploreIcon_jowccy.jpg?react",
+    // reelsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/reelzIcon_hjm9pg.jpg?react",
+    // messagesIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/msgIcon_ewypb9.jpg?react",
+    // notificationsIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531493/notificationIcon_b9nbe9.jpg?react",
+    // createIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531491/createIcon_rg9yc6.jpg?react",
+    // profileIcon: "",
+    // moreIcon: "https://res.cloudinary.com/dyg4ekmzg/image/upload/v1727531490/moreIcon_fwtgff.jpg?react",
 };
 
 export default sideMenuIcons;
--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -4,16 +4,16 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import sideMenuIcons from '../assets/svg/sideMenuIcons';
-// import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import ExploreRoundedIcon from '@mui/icons-material/ExploreRounded';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import MessageIcon from '@mui/icons-material/Message';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
+import TitleIcon from '../assets/svg/appTitleIcon.svg?react';
+import HomeIcon from '../assets/svg/homeIcon.svg?react';
+import SearchIcon from '../assets/svg/searchIcon.svg?react';
+import ExploreRoundedIcon from '../assets/svg/exploreRoundedIcon.svg?react';
+import ReelsIcon from '../assets/svg/reelsIcon.svg?react';
+import MessageIcon from '../assets/svg/msgIcon.svg?react';
+import NotificationsIcon from '../assets/svg/notificationsIcon.svg?react';
+import CreateIcon from '../assets/svg/createIcon.svg?react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; //TBD put logged in user profile pic
+import SettingsIcon from '../assets/svg/settingsIcon.svg?react';
 
 export function SideNav() {
     return (
@@ -28,60 +28,56 @@ export function SideNav() {
                 <ListItemButton component={Link} to="/instamode/homepage">
                     <ListItemIcon>
                     <img src="/img/instamode_logo.svg" style={{ width: '24px', height: '24px' }} />
+                    {/* <TitleIcon /> TBD - How i can change the name of the svg title*/}  
                     </ListItemIcon>
                     {/* <ListItemText primary="InstaMode" /> */}
                 </ListItemButton>
 
                 <ListItemButton component={Link} to="/instamode/homepage">
                     <ListItemIcon>
-                        <img src={sideMenuIcons.homeIcon} alt="Home" className="side-nav-icon" />
+                        <HomeIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Home" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.searchIcon} alt="Search" className="side-nav-icon search-icon" />
-                        <SearchIcon />
+                        <SearchIcon className="side-nav-icon search-icon"/>
                     </ListItemIcon>
                     <ListItemText primary="Search" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.exploreIcon} alt="Explore" className="side-nav-icon" />
-                        <ExploreRoundedIcon />
+                        <ExploreRoundedIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Explore" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.reelsIcon} alt="Reels" className="side-nav-icon"/>
-                        <PlayCircleOutlineIcon />
+                        <ReelsIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Reels" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.messagesIcon} alt="Messages" className="side-nav-icon" />
-                        <MessageIcon />
+                        <MessageIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Messages" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.notificationsIcon} alt="Notifications" className="side-nav-icon" />
+                        <NotificationsIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Notifications" />
                 </ListItemButton>
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.createIcon} alt="Create" className="side-nav-icon" />
-                        <AddBoxIcon />
+                        <CreateIcon className="side-nav-icon" />
                     </ListItemIcon>
                     <ListItemText primary="Create" />
                 </ListItemButton>
@@ -95,8 +91,7 @@ export function SideNav() {
 
                 <ListItemButton>
                     <ListItemIcon>
-                        <img src={sideMenuIcons.moreIcon} alt="More" className="side-nav-icon more-icon" />
-                        <MenuIcon />
+                        <SettingsIcon className="side-nav-icon more-icon" />
                     </ListItemIcon>
                     <ListItemText primary="More" />
                 </ListItemButton>
