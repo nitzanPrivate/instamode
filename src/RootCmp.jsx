8499fe7e4551fe0ce7060957a2a6ceb9d569081f
nitzan <nitzanp@hackeruso.com>
2024-11-23 16:47:37 +0200

    logger to rootCMP

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,15 +4,15 @@ import { Login } from './pages/Login.jsx';
 import { MainPage } from './pages/MainPage.jsx';
 
 export function RootCmp() {
-    console.log("Current Path:", window.location.pathname); // Log the current path
+    console.log("Current Path:", window.location.pathname); 
 
     return (
         <div>
             <main>
                 <Routes>
                     <Route path="/instamode/login" element={<Login />} />
-                    <Route path="/instamode/homepage" element={<MainPage />} /> {/* Updated to use MainPage */}
-                    <Route path="/" element={<Navigate to="/instamode/login" />} /> {/* Redirect root to login */}
+                    <Route path="/instamode/homepage" element={<MainPage />} /> 
+                    <Route path="/" element={<Navigate to="/instamode/login" />} /> 
                     <Route path="*" element={<Navigate to="/instamode/login" />} /> {/* Catch-all redirect to login */}
                 </Routes>
             </main>
