3be9d4e9061a42ec3be5834cd348e1407bc40173
nitzan <nitzanp@hackeruso.com>
2024-12-11 13:44:00 +0200

    removed story for now , fix page UI

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,30 +1,27 @@
-import React, { useEffect } from 'react';
-import { SideNav } from '../cmps/SideNav';
-import { StoryTopBar } from '../cmps/StoryTopBar';
-import { PostsList } from '../cmps/PostsList';
-import { mockPosts } from '../mockData/mockPosts.js';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { SideNav } from "../cmps/SideNav";
+import { StoryTopBar } from "../cmps/StoryTopBar";
+import { PostList } from "../cmps/PostList.jsx";
+import { mockPosts } from "../mockData/mockPosts.js";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function MainPage() {
-    const loggedInUser = useSelector(storeState => storeState.userModule.user);
-    const navigate = useNavigate();
+  const loggedInUser = useSelector((storeState) => storeState.userModule.user);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!loggedInUser) navigate('/instamode/login');
-    }, [loggedInUser, navigate]);
+  useEffect(() => {
+    if (!loggedInUser) navigate("/instamode/login");
+  }, [loggedInUser, navigate]);
 
-    if (!loggedInUser) return null;
+  if (!loggedInUser) return null;
 
-    return (
-        <section className="main-page" style={{ display: 'flex' }}>
-            <SideNav />
-            <div className="posts-section" style={{ marginLeft: '240px', padding: '20px', width: 'calc(100% - 240px)' }}>
-                 {/* <StoryTopBar /> */}
-                {mockPosts.map(post => (
-                    <PostsList key={post._id} post={post} />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="main-page">
+      <SideNav />
+      <div className="main-content">
+        <Outlet/>
+      </div>
+    </section>
+  );
 }
