20ad925705ce2a00b1f2eda8e6a5ad3b328afdc8
nitzan <nitzanp@hackeruso.com>
2024-12-11 13:39:00 +0200

    created nested root

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Login } from './pages/Login.jsx';
-import { MainPage } from './pages/MainPage.jsx';
-import { SearchPage } from './pages/SearchPage.jsx';
-import { ExplorePage } from './pages/ExplorePage.jsx';
-import { MessagesPage } from './pages/MessagesPage.jsx';
-import { NotificationsPage } from './pages/NotificationsPage.jsx';
-import { CreatePage } from './pages/CreatePage.jsx';
-import { SettingsPage } from './pages/SettingsPage.jsx';
-import { ProfilePage } from './pages/ProfilePage.jsx';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login.jsx";
+import { MainPage } from "./pages/MainPage.jsx";
+import { SearchPage } from "./pages/SearchPage.jsx";
+import { ExplorePage } from "./pages/ExplorePage.jsx";
+import { MessagesPage } from "./pages/MessagesPage.jsx";
+import { NotificationsPage } from "./pages/NotificationsPage.jsx";
+import { CreatePage } from "./pages/CreatePage.jsx";
+import { SettingsPage } from "./pages/SettingsPage.jsx";
+import { ProfilePage } from "./pages/ProfilePage.jsx";
+import { PostIndex } from "./cmps/PostIndex.jsx";
 
 export function RootCmp() {
-    console.log("Current Path:", window.location.pathname);
+  console.log("Current Path:", window.location.pathname);
 
-    return (
-        <div>
-            <main>
-                <Routes>
-                    <Route path="/instamode/login" element={<Login />} />
-                    <Route path="/instamode/homepage" element={<MainPage />} />
-                    <Route path="/instamode/search" element={<SearchPage />} />
+  return (
+    <div>
+      <main>
+        <Routes>
+          <Route path="/instamode/login" element={<Login />} />
+          <Route path="/instamode" element={<MainPage />}>
+            <Route path="post" element={<PostIndex />} />
+            <Route path="explore" element={<ExplorePage />} />
+            <Route path=":username" element={<ProfilePage />} /> 
+          </Route>
+          {/* <Route path="/instamode/create" element={<CreatePage />} /> */}
+          {/* <Route path="/instamode/search" element={<SearchPage />} />
                     <Route path="/instamode/explore" element={<ExplorePage />} />
-                    {/* <Route path="/instamode/reels" element={<ReelsPage />} /> */}
                     <Route path="/instamode/messages" element={<MessagesPage />} />
                     <Route path="/instamode/notifications" element={<NotificationsPage />} />
-                    <Route path="/instamode/create" element={<CreatePage />} />
                     <Route path="/instamode/settings" element={<SettingsPage />} />
-                    <Route path="/instamode/:username" element={<ProfilePage />} />
-                    <Route path="/" element={<Navigate to="/instamode/login" />} />
-                    <Route path="*" element={<Navigate to="/instamode/login" />} /> {/* Catch-all redirect to login */}
-                </Routes>
-            </main>
-        </div>
-    );
+                    <Route path="/instamode/:username" element={<ProfilePage />} /> */}
+          <Route path="/" element={<Navigate to="/instamode/login" />} />
+          <Route path="*" element={<Navigate to="/instamode/login" />} />{" "}
+          {/* Catch-all redirect to login */}
+        </Routes>
+      </main>
+    </div>
+  );
 }
